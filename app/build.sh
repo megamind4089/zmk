8fb1dcda00f8d745b1fb68b5e3c4e0ca1a47c9e1
Mega Mind <68985133+megamind4089@users.noreply.github.com>
2020-09-18 20:57:28 +0800

    Added build script

---
+++ app/build.sh
@@ -0,0 +1,5 @@
+# west build -d build/left  -b nrfmicro_13 -- -DSHIELD=lily58_left
+# west build --pristine -t menuconfig -d build/right -b nrfmicro_13 -- -DSHIELD=lily58_right -DKEYMAP=default
+# west build -d build/right -b nrfmicro_13 -- -DSHIELD=lily58_right
+west build -d build -b nrfmicro_13 -- -DSHIELD=lily58_left
+cp ./build/zephyr/zmk.uf2 /run/media/maari/NRF52BOOT
