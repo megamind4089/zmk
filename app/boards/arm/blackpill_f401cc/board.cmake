3ce543e4d1d7bc6b63f40c46116af18c6f0c6c99
Mega Mind <68985133+megamind4089@users.noreply.github.com>
2020-09-26 20:08:13 +0800

    Added bluepill board

--- app/boards/arm/blackpill_f401cc/board.cmake
+++ app/boards/arm/blackpill_f401cc/board.cmake
@@ -2,7 +2,9 @@
 
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
 board_runner_args(jlink "--device=STM32F401CC" "--speed=8000")
+board_runner_args(blackmagicprobe "--gdb-serial=/dev/ttyBmpGdb")
 
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
